c51f7864d8c6df3a8e0801a42b4765af3948b2df
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2023-03-30 21:01:09 -0700

    base components

---
+++ index.js
@@ -0,0 +1,13 @@
+const save = document.getElementById("save");
+const open = document.getElementById("open");
+const quit = document.getElementById("quit");
+const undo = document.getElementById("undo");
+const paste = document.getElementById("paste");
+const support = document.getElementById("support");
+const feedback = document.getElementById("feedback");
+const improve = document.getElementById("improve");
+const about = document.getElementById("about");
+
+about.addEventListener("click", () => {
+    window.open("https://github.com/");
+});
