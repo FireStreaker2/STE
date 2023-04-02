9194cfe17d59e8bd2507d423c1144d38c908e793
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2023-04-02 16:13:55 -0700

    ste!!

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# STE
- A VERY simple text editor for the web.
+![STE](https://socialify.git.ci/FireStreaker2/STE/image?description=1&font=Jost&forks=1&issues=1&logo=https%3A%2F%2Fste.firestreaker2.gq%2Ffavicon.png&name=1&owner=1&pattern=Solid&pulls=1&stargazers=1&theme=Dark)
+
+# What is STE?
+STE is a VERY simple text editor for the web. As implied from the description, STE stands for ``Simple Text Editor``.
+
+## Contributing
+If you would like to contribute, you can <a href='https://github.com/FireStreaker2/STE/fork'>fork the repo</a> and <a href='https://github.com/FireStreaker2/STE/compare'>make a PR</a>, or contact me via email @ ``suggestions@firestreaker2.gq``
+
+## License
+<a href="https://github.com/FireStreaker2/STE/blob/main/LICENSE">MIT</a>
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const type = document.getElementById("type");
 const save = document.getElementById("save");
 const open = document.getElementById("open");
+const fileOpen = document.getElementById("fileOpen");
 const quit = document.getElementById("quit");
 const undo = document.getElementById("undo");
 const paste = document.getElementById("paste");
@@ -8,6 +10,77 @@ const feedback = document.getElementById("feedback");
 const improve = document.getElementById("improve");
 const about = document.getElementById("about");
 
+save.addEventListener("click", () => {
+    var info = type.value;
+    var fileName = "steDocument.txt";
+    var fileType = "text/plain";
+
+    var blob = new Blob([info], { type: fileType });
+    var url = URL.createObjectURL(blob);
+
+    var link = document.createElement("a");
+    link.href = url;
+    link.download = fileName;
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+});
+
+open.addEventListener("click", () => {
+    fileOpen.click();
+
+    fileOpen.addEventListener("change", () => {
+        var file = fileOpen.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function() {
+            var fileContents = reader.result;
+            type.value = fileContents;
+        };
+
+        reader.readAsText(file);
+        alert("File has been succesfully loaded.");
+    });
+});
+
+quit.addEventListener("click", () => {
+    var confirmed = window.confirm("Are you sure you want to quit?");
+
+    if (!confirmed) {
+        return;
+    }
+    
+    window.close();
+});
+
+undo.addEventListener("click", () => {
+    var currentValue = type.value;
+    var lastLineBreakIndex = currentValue.lastIndexOf("\n");
+    var newValue = currentValue.substring(0, lastLineBreakIndex);
+    type.value = newValue;
+});
+
+paste.addEventListener("click", () => {
+    navigator.clipboard.readText().then(function(clipboardText) {
+        type.value += clipboardText;
+    });
+});
+
+support.addEventListener("click", () => {
+    window.open("https://github.com/FireStreaker2/STE/issues");
+});
+
+feedback.addEventListener("click", () => {
+    window.open("https://github.com/FireStreaker2/STE/issues");
+});
+
+improve.addEventListener("click", () => {
+    window.open("https://github.com/FireStreaker2/STE/pulls");
+});
+
 about.addEventListener("click", () => {
-    window.open("https://github.com/");
+    window.open("https://github.com/FireStreaker2/STE");
 });
